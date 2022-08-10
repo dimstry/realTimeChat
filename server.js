89a9aef648180c4b592279dc8b0bed2b785fd73c
dimstry <dimastriana03@gmail.com>
2022-08-10 19:50:31 +0700

    update server

--- server.js
+++ server.js
@@ -6,21 +6,27 @@ const { Server } = require("socket.io");
 const io = new Server(server);
 
 const port = 3000;
-
+// jika server di buka
 app.get('/', (req, res) => {
+    // directory yang di perlihatkan
     res.sendFile(__dirname + '/src/index.html');
 });
 
 io.on('connection', (socket) => {
+    // socket on dapatkan masage
     socket.on('chat message', (msg) => {
+        console.log(msg)
+        // emit massage
         io.emit('chat message', msg);
     });
+    // jika user disconect
     socket.on('disconnect', () => {
+
         console.log('user disconnected');
     });
 });
 
 
 server.listen(3000, () => {
-console.log(`listening on *:${port}`);
+    console.log(`listening on *:${port}`);
 });
